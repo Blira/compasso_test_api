a26f90f50c90498745169689a23c9c4f3b49f556
Bruno Lira <brunoelira@gmail.com>
2021-04-18 00:12:52 -0300

    feat: create client repository

--- src/data/protocols/client-repository.ts
+++ src/data/protocols/client-repository.ts
@@ -1,6 +1,8 @@
 import { ClientModel } from '../../domain/models/client';
+import { FetchClientFilter } from '../../domain/usecases/client/fetch-client';
 import { RegisterClientModel } from '../../domain/usecases/client/register-client';
 
 export interface ClientRepository {
   insert(registerClientData: RegisterClientModel): Promise<ClientModel>;
+  find(fetchClientFilter: FetchClientFilter): Promise<ClientModel[]>;
 }
