02459e5035fd3514a51e21b7e534df31a81e437e
Bruno Lira <brunoelira@gmail.com>
2021-04-18 00:14:42 -0300

    feat: implement fetch client repository

--- src/infra/db/postgres/repositories/client/client-repository.ts
+++ src/infra/db/postgres/repositories/client/client-repository.ts
@@ -1,10 +1,22 @@
+import { Repository } from 'typeorm';
 import { ClientRepository } from '../../../../../data/protocols/client-repository';
 import { ClientModel } from '../../../../../domain/models/client';
+import { FetchClientFilter } from '../../../../../domain/usecases/client/fetch-client';
 import { RegisterClientModel } from '../../../../../domain/usecases/client/register-client';
 import { DatabaseConnection } from '../../../../../main/instances-container';
+import { CityEntity } from '../../entities/city';
 import { ClientEntity } from '../../entities/client';
 
 export class PostgresClientRepository implements ClientRepository {
+  private clientRepository: Repository<ClientEntity>;
+  constructor() {
+    DatabaseConnection.getRepository<ClientEntity>('clients')
+      .then(repository => {
+        this.clientRepository = repository;
+      })
+      .catch(e => console.log(e));
+  }
+
   async insert(registerClientData: RegisterClientModel): Promise<ClientModel> {
     const clientRepository = await DatabaseConnection.getRepository<ClientEntity>(
       'clients',
@@ -13,5 +25,10 @@ export class PostgresClientRepository implements ClientRepository {
     const newClient = clientRepository.create(registerClientData);
     await clientRepository.save(newClient);
     return newClient;
+  }
+
+  async find(fetchClientFilter: FetchClientFilter): Promise<ClientModel[]> {
+    const cities = await this.clientRepository.find(fetchClientFilter);
+    return cities;
   }
 }
