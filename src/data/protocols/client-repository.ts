91ba9963b183ec98cc6bd196f8bbfa3dbfbbc9ab
Bruno Lira <brunoelira@gmail.com>
2021-04-18 01:05:48 -0300

    feat: add update to ClientRepository

--- src/data/protocols/client-repository.ts
+++ src/data/protocols/client-repository.ts
@@ -1,9 +1,11 @@
 import { ClientModel } from '../../domain/models/client';
 import { FetchClientFilter } from '../../domain/usecases/client/fetch-client';
 import { RegisterClientModel } from '../../domain/usecases/client/register-client';
+import { UpdateClientData } from '../../domain/usecases/client/update-client';
 
 export interface ClientRepository {
   insert(registerClientData: RegisterClientModel): Promise<ClientModel>;
+  update(updateClientData: UpdateClientData): Promise<ClientModel>;
   find(fetchClientFilter: FetchClientFilter): Promise<ClientModel[]>;
   delete(client_id: string): Promise<void>;
 }
