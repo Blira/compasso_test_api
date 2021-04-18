0984694c3e811aab6bc3a154dc767706fade3b91
Bruno Lira <brunoelira@gmail.com>
2021-04-17 22:03:46 -0300

    feat: create client migration

---
+++ src/infra/db/postgres/migrations/1618707024916-CreateClientTable.ts
@@ -0,0 +1,67 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
+
+export class CreateClientTable1618707024916 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query("CREATE TYPE sexEnum AS ENUM ('M', 'F');");
+    await queryRunner.createTable(
+      new Table({
+        name: 'clients',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'sex',
+            type: 'sexEnum',
+          },
+          {
+            name: 'age',
+            type: 'integer',
+          },
+          {
+            name: 'city_id',
+            type: 'uuid',
+            isUnique: true,
+          },
+          {
+            name: 'birthDate',
+            type: 'timestamp',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'clientCity',
+            referencedTableName: 'cities',
+            referencedColumnNames: ['id'],
+            columnNames: ['city_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('clients');
+  }
+}
