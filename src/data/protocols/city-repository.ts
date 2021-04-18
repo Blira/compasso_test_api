cb703298a6d7450d6c5324624717d5a15c21b040
Bruno Lira <brunoelira@gmail.com>
2021-04-17 23:07:16 -0300

    feat: add find to cityrepository

--- src/data/protocols/city-repository.ts
+++ src/data/protocols/city-repository.ts
@@ -1,6 +1,8 @@
 import { CityModel } from '../../domain/models/city';
+import { FetchCityFilter } from '../../domain/usecases/city/fetch-city';
 import { RegisterCityModel } from '../../domain/usecases/city/register-city';
 
 export interface CityRepository {
   insert(registerCityData: RegisterCityModel): Promise<CityModel>;
+  find(fetchCityFilter: FetchCityFilter): Promise<CityModel[]>;
 }
