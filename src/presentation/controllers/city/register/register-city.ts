1c6b57b35c0679d6ba2daba335991c0502a28ddf
Bruno Lira <brunoelira@gmail.com>
2021-04-17 21:19:32 -0300

    feat: create RegisterCityController

---
+++ src/presentation/controllers/city/register/register-city.ts
@@ -0,0 +1,37 @@
+import { RegisterCity } from '../../../../domain/usecases/city/register-city';
+import { missingParamError } from '../../../helpers/httpHelper';
+import { Controller } from '../../../protocols/controller';
+import { HttpRequest, HttpResponse } from '../../../protocols/http';
+
+export class RegisterCityController implements Controller {
+  private readonly registerCity: RegisterCity;
+
+  constructor(registerCity: RegisterCity) {
+    this.registerCity = registerCity;
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { body: registerCityData } = httpRequest;
+
+      const requiredParams = ['name', 'state'];
+
+      for (const param of requiredParams) {
+        if (!registerCityData[param]) {
+          return missingParamError(param);
+        }
+      }
+
+      const newCity = await this.registerCity.register(registerCityData);
+      return {
+        statusCode: 201,
+        body: newCity,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new Error('Internal server error'),
+      };
+    }
+  }
+}
