8926b1b4c464857ea3d28f7687742c123d14bd84
Bruno Lira <brunoelira@gmail.com>
2021-04-17 23:11:48 -0300

    feat: create FetchCityController factory

--- src/main/factories/city.ts
+++ src/main/factories/city.ts
@@ -1,9 +1,18 @@
+import { DbFetchCity } from '../../data/usecases/city/fetch-city/db-fetch-city';
 import { DbRegisterCity } from '../../data/usecases/city/register-city/db-register-city';
+import { FetchCityFilter } from '../../domain/usecases/city/fetch-city';
 import { PostgresCityRepository } from '../../infra/db/postgres/repositories/city/city-repository';
+import { FetchCityController } from '../../presentation/controllers/city/fetch/fetch-city';
 import { RegisterCityController } from '../../presentation/controllers/city/register/register-city';
 
 export const createRegisterCityController = (): RegisterCityController => {
   const cityRepository = new PostgresCityRepository();
   const registerCity = new DbRegisterCity(cityRepository);
   return new RegisterCityController(registerCity);
+};
+
+export const createFetchCityController = (): FetchCityController => {
+  const cityRepository = new PostgresCityRepository();
+  const fetchCity = new DbFetchCity(cityRepository);
+  return new FetchCityController(fetchCity);
 };
