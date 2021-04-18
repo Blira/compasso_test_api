cf9d458d3429f458dd58c26eeabdb07591af556a
Bruno Lira <brunoelira@gmail.com>
2021-04-18 01:07:23 -0300

    feat: add createUpdateClientController to factory

--- src/main/factories/client.ts
+++ src/main/factories/client.ts
@@ -1,11 +1,12 @@
 import { DbFetchClient } from '../../data/usecases/client/fetch-client/db-fetch-client';
 import { DbRegisterClient } from '../../data/usecases/client/register-client/db-register-client';
 import { DbRemoveClient } from '../../data/usecases/client/remove-client/db-remove-client';
-import { FetchClientFilter } from '../../domain/usecases/client/fetch-client';
+import { DbUpdateClient } from '../../data/usecases/client/update-client/db-update-client';
 import { PostgresClientRepository } from '../../infra/db/postgres/repositories/client/client-repository';
 import { FetchClientController } from '../../presentation/controllers/client/fetch/fetch-client';
 import { RegisterClientController } from '../../presentation/controllers/client/register/register-client';
 import { RemoveClientController } from '../../presentation/controllers/client/remove/remove-client';
+import { UpdateClientController } from '../../presentation/controllers/client/update/update-client';
 
 export const createRegisterClientControlller = (): RegisterClientController => {
   const clientRepository = new PostgresClientRepository();
@@ -18,8 +19,15 @@ export const createFetchClientController = (): FetchClientController => {
   const fetchClient = new DbFetchClient(clientRepository);
   return new FetchClientController(fetchClient);
 };
+
 export const createRemoveClientController = (): RemoveClientController => {
   const clientRepository = new PostgresClientRepository();
   const removeClient = new DbRemoveClient(clientRepository);
   return new RemoveClientController(removeClient);
+};
+
+export const createUpdateClientController = (): UpdateClientController => {
+  const clientRepository = new PostgresClientRepository();
+  const updateClient = new DbUpdateClient(clientRepository);
+  return new UpdateClientController(updateClient);
 };
