43e106be9d421bade3975c581e25e34fe78a0483
Bruno Lira <brunoelira@gmail.com>
2021-04-17 23:08:02 -0300

    feat: add find to cityrepository

--- src/infra/db/postgres/repositories/city/city-repository.ts
+++ src/infra/db/postgres/repositories/city/city-repository.ts
@@ -1,17 +1,29 @@
+import { Repository } from 'typeorm';
 import { CityRepository } from '../../../../../data/protocols/city-repository';
 import { CityModel } from '../../../../../domain/models/city';
+import { FetchCityFilter } from '../../../../../domain/usecases/city/fetch-city';
 import { RegisterCityModel } from '../../../../../domain/usecases/city/register-city';
 import { DatabaseConnection } from '../../../../../main/instances-container';
 import { CityEntity } from '../../entities/city';
 
 export class PostgresCityRepository implements CityRepository {
-  async insert(registerCityData: RegisterCityModel): Promise<CityModel> {
-    const cityRepository = await DatabaseConnection.getRepository<CityEntity>(
-      'cities',
-    );
+  private cityRepository: Repository<CityEntity>;
+  constructor() {
+    DatabaseConnection.getRepository<CityEntity>('cities')
+      .then(repository => {
+        this.cityRepository = repository;
+      })
+      .catch(e => console.log(e));
+  }
 
-    const newCity = cityRepository.create(registerCityData);
-    await cityRepository.save(newCity);
+  async insert(registerCityData: RegisterCityModel): Promise<CityModel> {
+    const newCity = this.cityRepository.create(registerCityData);
+    await this.cityRepository.save(newCity);
     return newCity;
+  }
+
+  async find(fetchCityFilter: FetchCityFilter): Promise<CityModel[]> {
+    const cities = await this.cityRepository.find(fetchCityFilter);
+    return cities;
   }
 }
